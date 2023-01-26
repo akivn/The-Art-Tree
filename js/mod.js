c2c87e6ba447f91746042eb369b9b91d91415615
akivn <patrickkeroro226@gmail.com>
2023-01-26 18:46:21 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -5,22 +5,23 @@ let modInfo = {
 	pointsName: "skill",
 	modFiles: ["layers/art.js", "layers/rein.js", "layers/chal.js", "layers/inf.js", "layers/achievement.js", "tree.js"],
 
-	discordName: "",
-	discordLink: "",
+	discordName: "neuro sama your reality (X)",
+	discordLink: "https://discord.gg/h6TFgujqtN",
 	initialStartPoints: new Decimal (0), // Used for hard resets and new players
 	offlineLimit: 4,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "A0.1",
-	name: "Literally nothing",
+	num: "A0.11",
+	name: "Infinity Update 1",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+		- Added Infinity!<br>
+		- Reach 1.797e308 Art Points to prestige for Infinity points, and use it in Generators which generates Reincaranations, or in upgrades!<br>
+		- Endgame increased to 1e15 Infinity Points.`
 
 let winText = `Congratulations! You have became the new King of Art and beaten this game, but for now...`
 
